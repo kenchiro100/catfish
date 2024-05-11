b865498227807c30a0eac9adb27e2385e37c910c
kenzoobryan <kenzobresme@gmail.com>
2024-05-12 00:19:56 +0100

    basic error handling

--- ALGO.md
+++ ALGO.md
@@ -1,4 +1,4 @@
 Le principe est de recuperer des informations de bases par rapport au site qui est sur la page
 * Recuperer l'Url deja et verifier si le protocole est "http" ou "https"
 - si l'url est en protocole http, une variable nommee doutes s'incremente de 1
-* Verifier la taille du code et verifier si il y a des urls derriere , par exemple
+* Verifier la taille du code et verifier si il y a des urls derriere ,par exemple.
--- src/checker/check.py
+++ src/checker/check.py
@@ -3,12 +3,20 @@ from bs4 import BeautifulSoup
 import urllib
 import sys
 from urllib.parse import urlparse
+
 url = sys.argv[1]  # Recuperer le lien du site web
 response = requests.get(url)
 
 #contient 
 if response.status_code == 200:
     html_content = response.content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.find_all('a')
+
+    for link in links:
+        href = link.get('href')
+        text = link.text
+        print(text, href)
     # Traitez le contenu HTML ici
 else:
     print("La requête a échoué avec le code d'état :", response.status_code)
@@ -18,10 +26,10 @@ else:
 
 # print(base_url)
 
-soup = BeautifulSoup(html_content, 'html.parser')
-links = soup.find_all('a')
+# soup = BeautifulSoup(html_content, 'html.parser')
+# links = soup.find_all('a')
 
-for link in links:
-    href = link.get('href')
-    text = link.text
-    print(text, href)
+# for link in links:
+#     href = link.get('href')
+#     text = link.text
+#     print(text, href)
