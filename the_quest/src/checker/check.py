08aa4339b6eb21a63b3b558b9eee9cec7c3ebc51
kenzoobryan <kenzobresme@gmail.com>
2024-05-11 14:27:50 +0100

    debut de quelque chose

---
+++ the_quest/src/checker/check.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib
+import sys
+from urllib.parse import urlparse
+url = sys.argv[1]  # Recuperer le lien du site web
+response = requests.get(url)
+
+#contient 
+if response.status_code == 200:
+    html_content = response.content
+    # Traitez le contenu HTML ici
+else:
+    print("La requête a échoué avec le code d'état :", response.status_code)
+
+# parsed_url = urlparse(url)
+# base_url = parsed_url.scheme + "://" + parsed_url.netloc
+
+# print(base_url)
+
+soup = BeautifulSoup(html_content, 'html.parser')
+links = soup.find_all('a')
+
+for link in links:
+    href = link.get('href')
+    text = link.text
+    print(text, href)
